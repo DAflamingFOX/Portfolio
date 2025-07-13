b2c381e756d7dee97b53ebbb992651cda3a5cb72
Jeffrey Morris <ffejcsirrom@gmail.com>
2025-07-12 20:02:46 -0500

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { Banner, Head, Search } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
@@ -63,6 +64,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             {children}
           </Providers>
         </Layout>
+        <Analytics/>
       </body>
     </html>
   )
