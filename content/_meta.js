9240b1e164c4e09bfe5745a929ef93a84d1e5d80
Jeffrey Morris <ffejcsirrom@gmail.com>
2025-07-13 00:55:08 -0500

    Add pagination to pages

--- content/_meta.js
+++ content/_meta.js
@@ -1,19 +1,46 @@
 export default {
     'index': {
+        title: 'Home',
         theme: {
             breadcrumb: false,
             sidebar: false,
             toc: false,
-            pagination: false
+            pagination: true
         }
     },
     portfolio: {
+        theme: {
+            breadcrumb: false,
+            sidebar: false
+        }
+    },
+    portfolio_link: {
         type: 'page',
+        title: 'Portfolio',
+        href: '/portfolio'
     },
     resume: {
+        title: 'Résumé',
+        theme: {
+            breadcrumb: false,
+            sidebar: false
+        }
+    },
+    resume_link: {
         type: 'page',
+        title: 'Résumé',
+        href: '/resume'
     },
     contact_me: {
+        title: 'Contact Me',
+        theme: {
+            breadcrumb: false,
+            sidebar: false
+        }
+    },
+    contact_me_link: {
         type: 'page',
+        title: 'Contact Me',
+        href: '/contact_me'
     }
 }
