314fa879e724997015538dddf288546dcada2e3d
Jeffrey Morris <ffejcsirrom@gmail.com>
2025-09-25 23:58:02 -0500

    Add a buisness card redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+            // Buisness card redirect.
+            {
+                source: '/biz_card',
+                destination: '/',
+                permanent: true,
+            },
 
+        ]
+    },
 }
 
 // Merge MDX config with Next.js config
