1435bc8d4b8a0b6328e53c3aa0c14514643167a5
Jeffrey Morris <ffejcsirrom@gmail.com>
2026-02-23 11:48:20 -0600

    Fix article header tags

--- app/blog/[slug]/ArticleHeader.tsx
+++ app/blog/[slug]/ArticleHeader.tsx
@@ -12,7 +12,7 @@ export default function ArticleHeader({ metadata }: Props) {
     return (
         <>
             <div className='flex flex-col gap-y-0.5 mb-4'>
-                <div className='flex flex-row justify-between items-baseline'>
+                <div className='flex flex-col md:flex-row justify-between items-baseline'>
                     <h4 className='text-secondary-600'>
                         {new Date(metadata.date).toLocaleDateString([], {
                             year: 'numeric',
@@ -20,7 +20,7 @@ export default function ArticleHeader({ metadata }: Props) {
                             day: 'numeric'
                         })}
                     </h4>
-                    <div className="flex flex-row gap-x-2">
+                    <div className="flex flex-row flex-wrap gap-2">
                         {metadata.tags.map((tag) => (
                             <Link key={tag} href={`/blog/tag/${tag}`}>
                                 <Chip key={tag} variant='bordered' color='secondary'>{tag}</Chip>
