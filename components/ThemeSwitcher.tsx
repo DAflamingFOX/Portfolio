c356f27ed1e3a13582357cea9e7f42025e483fe7
Jeffrey Morris <ffejcsirrom@gmail.com>
2026-02-23 11:26:13 -0600

    Remove old commented code

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -60,14 +60,4 @@ export function ThemeSwitcher() {
         </Button>
     )
 
-
-
-
-    // return (
-    //     <div>
-    //         The current theme is: {theme}
-    //         <button onClick={() => setTheme('light')}>Light Mode</button>
-    //         <button onClick={() => setTheme('dark')}>Dark Mode</button>
-    //     </div>
-    // )
 };
